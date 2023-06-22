6b0d49130a2a0c919d388b5674eb7486cb032ed9
omkashyap7 <60043283+omkashyap7@users.noreply.github.com>
2023-06-22 10:12:46 +0530

    Delete behave.js

--- behave.js
+++
@@ -1,7 +0,0 @@
-const ham = document.querySelector('.hamburger');
-const nav_menus = document.querySelector('.nav-menu');
-
-ham.addEventListener("click", ()=>{
-    ham.classList.toggle("active");
-    nav_menus.classList.toggle("active");
-})
